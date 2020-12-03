a67c5eb9ccd6131b59416af7cc813da859f9d1ff
rafag0n <rafaelpacg@gmail.com>
2020-12-03 19:19:40 -0300

    refactor header tests for better reading

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,11 +2,12 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../pages/index';
 
-const sidenavOpen = 'sidenav__open';
-const sidenavClose = 'sidenav__close';
-const sidenavContents = 'sidenav__contents';
-const hidden = '--hidden';
-const visible = '--visible';
+const get = (id) => () => screen.getByTestId(id);
+const sidenavOpen = get('sidenav__open');
+const sidenavClose = get('sidenav__close');
+const sidenavContents = get('sidenav__contents');
+const sidenavHidden = 'sidenav__contents--hidden';
+const sidenavVisible = 'sidenav__contents--visible';
 
 describe('Header', () => {
 	beforeEach(() => {
@@ -22,26 +23,16 @@ describe('Header', () => {
 	});
 
 	it('Opens menu properly', () => {
-		expect(screen.getByTestId(sidenavContents)).toHaveClass(
-			sidenavContents.concat(hidden)
-		);
-		fireEvent.click(screen.getByTestId(sidenavOpen));
-		expect(screen.getByTestId(sidenavContents)).toHaveClass(
-			sidenavContents.concat(visible)
-		);
+		expect(sidenavContents()).toHaveClass(sidenavHidden);
+		fireEvent.click(sidenavOpen());
+		expect(sidenavContents()).toHaveClass(sidenavVisible);
 	});
 
 	it('Closes menu properly', () => {
-		expect(screen.getByTestId(sidenavContents)).toHaveClass(
-			sidenavContents.concat(hidden)
-		);
-		fireEvent.click(screen.getByTestId(sidenavOpen));
-		expect(screen.getByTestId(sidenavContents)).toHaveClass(
-			sidenavContents.concat(visible)
-		);
-		fireEvent.click(screen.getByTestId(sidenavClose));
-		expect(screen.getByTestId(sidenavContents)).toHaveClass(
-			sidenavContents.concat(hidden)
-		);
+		expect(sidenavContents()).toHaveClass(sidenavHidden);
+		fireEvent.click(sidenavOpen());
+		expect(sidenavContents()).toHaveClass(sidenavVisible);
+		fireEvent.click(sidenavClose());
+		expect(sidenavContents()).toHaveClass(sidenavHidden);
 	});
 });
